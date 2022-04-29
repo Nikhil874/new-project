b150b3ca4d3e74260fb19f413a7181b4d305a8f5
Nikhil874 <95832751+Nikhil874@users.noreply.github.com>
2022-04-29 23:46:05 +0530

    update-forgot password

--- src/middlewares/hash.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require("bcryptjs");
-module.exports=(req,res,next)=>{
-    console.log(req)
-    // var hash=bcrypt.hashSync(req.body.password,8);
-    // req.body.password=hash;
-    return next();
-}
