6aaa42cc278d476351f75e3ec38ca6fb981eae81
Nikhil874 <95832751+Nikhil874@users.noreply.github.com>
2022-04-29 12:44:23 +0530

    database connect

---
+++ src/config/db.js
@@ -0,0 +1,6 @@
+//api username and password with unique id(email id)
+const mongoose=require("mongoose");
+
+module.exports=()=>{
+    mongoose.connect("mongodb+srv://Nikhil874:Nikil874@cluster0.3l0um.mongodb.net/newLogin");
+}
---
+++ src/model/user.model.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose");
+//api username and password with unique id(email id)
+const userSchema=new mongoose.Schema({
+    user_name:{type:String,required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true},
+},{
+    versionKey:false,
+    timestamps:true
+})
+
+module.exports=mongoose.model("user",userSchema);
+
